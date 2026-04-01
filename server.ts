b3142d66f5e88991a4ad506a4c48de6939e0c1b2
hetpatel0208 <het0208patel@gmail.com>
2026-04-01 19:45:37 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -347,8 +347,10 @@ async function startServer() {
     });
   }
 
-  httpServer.listen(3000, "0.0.0.0", () => {
-    console.log("Server running on http://localhost:3000");
+  const PORT = process.env.PORT || 3000;
+  
+  httpServer.listen(PORT as number, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
